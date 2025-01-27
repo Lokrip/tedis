e51d150e008f51ce6157e6ca86a8e0201500613b
Lokrip <165576212+Lokrip@users.noreply.github.com>
2025-01-27 16:25:46 +0600

    feat: add ssr render

--- src/components/layouts/Main/Store/ContentWrapper/ContentWrapper.tsx
+++ src/components/layouts/Main/Store/ContentWrapper/ContentWrapper.tsx
@@ -2,40 +2,27 @@ import {FC} from 'react';
 import styles from './contentWrapper.module.scss';
 import PCard from '../../../../ui/elements/card/PrimaryCard/PrimaryCard';
 import { getProductsData } from '../../../../../service/api/product.api';
+import Pagination from '../../Pagination/Pagination';
+import { IPaginationProduct } from '@/types/app/models/IProduct.type';
 
 interface ContentWrapperProps {
     param?: any;
 }
 
 const ContentWrapper: FC<ContentWrapperProps> = async ({param}) => {
+    const LIMIT = 18
     const searchQuery = param.q ?? param.searchQuery;
     const currentPage = Number(param?.page) || 1;
 
-    let content;
-    
-    try {
-        const data = await getProductsData(searchQuery, {
-            isPagination: true,
-            currentPage,
-        });
-        content = <>{(data as any).map((item: any) => (
-            <PCard key={item.slug} item={item} />
-        ))}</>
-    } catch(error) {
-        content = (
-            <div className={styles.productContentWrapper}>
-                <p>Произошла ошибка при загрузке продуктов. Пожалуйста, попробуйте позже.</p>
-            </div>
-        )
-    }
+    const {results} = await getProductsData<IPaginationProduct>(searchQuery)
 
     return (
         <div className={styles.productContentWrapper}>
-            {content || (
-                <div className={styles.loading}>
-                    <p>Загрузка продуктов...</p>
-                </div>
-            )}
+            {results.map(item => (
+                <PCard key={item.slug} item={item} />
+            ))}
+
+            {/* <Pagination limit={LIMIT} currentPage={currentPage}  /> */}
         </div>
     )
 };
--- src/service/api/product.api.ts
+++ src/service/api/product.api.ts
@@ -1,15 +1,28 @@
 import { IPost } from "../../types/app/models/IPost.type";
 import { axios } from "../axios"
 
-async function getProductsData(search: string | null, {
+async function getProductsData<T>(search: string | null, {
     isPagination = false,
     currentPage = null
 }: {
     isPagination?: boolean;
     currentPage?: null | number
-}) {
+} = {}): Promise<T> {
     try {
-        const data = await axios.get<IPost[]>(`/api/v1/product/${search ? `?q=${search}` : ""}`)
+        let url = "/api/v1/product/"
+
+        const params = [];
+
+        if(search)
+            params.push(`q=${search}`)
+
+        if (isPagination && currentPage)
+            params.push(`page=${currentPage}`);
+
+        if (params.length > 0)
+            url += `?${params.join("&")}`;
+        
+        const data = await axios.get<T>(url);
         return data;
     } catch(error) {
         console.error(error)
