631b6d3346781a0d42e1ca1d43420cd379b52131
Lokrip <165576212+Lokrip@users.noreply.github.com>
2025-02-05 13:47:48 +0600

    refactor: improved product output

--- src/components/layouts/Main/Store/ContentWrapper/ContentWrapper.tsx
+++ src/components/layouts/Main/Store/ContentWrapper/ContentWrapper.tsx
@@ -3,6 +3,8 @@ import styles from './contentWrapper.module.scss';
 import { IPaginationProduct } from '@/types/app/models/IProduct.type';
 import ProductList from './ProductList/ProductList';
 import { getProductsData } from '@/core/api/product.api';
+import { ProductError } from '@/types/app/enum/product.enum';
+import { HeadingH } from '@/components/plagins/H.number';
 
 interface ContentWrapperProps {
     param?: any;
@@ -12,21 +14,32 @@ const ContentWrapper: FC<ContentWrapperProps> = async ({param}) => {
     const searchQuery = param.q ?? param.searchQuery;
     const currentPage = Number(param?.page) || 1;
 
-    const {results: products, count: productCount} = await getProductsData<IPaginationProduct>(
-        searchQuery,
-        {isPagination: true,
-        currentPage: currentPage}
-    )
+    try {
+        const {results: products, count: totalProductCount} = await getProductsData<IPaginationProduct>(
+            searchQuery,
+            {isPagination: true,
+            currentPage: currentPage}
+        )
 
-    return (
-        <ProductList
-            searchQuery={searchQuery}
-            totalProductCount={productCount}
-            initialProducts={products}
-            getProductsData={getProductsData}
-            classNameListDataContainer={styles.productContentWrapper}
-        />
-    )
+        return (
+            <ProductList
+                searchQuery={searchQuery}
+                totalProductCount={totalProductCount}
+                initialProducts={products}
+                getProductsData={getProductsData}
+                classNameListDataContainer={styles.productContentWrapper}
+            />
+        );
+    } catch(error) {
+        const productNotFound = ProductError.ProductNotFound
+
+        return (
+            <HeadingH
+                level={2}
+                content={productNotFound ?? "Data list not available"}
+            />
+        );
+    }
 };
 
 export default ContentWrapper;
