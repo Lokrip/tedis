cec95fbc19e33f8fe4d5e772ac463d68d6ce4d1d
Lokrip <165576212+Lokrip@users.noreply.github.com>
2025-02-08 21:19:10 +0600

    feat: add Category filter

--- server/serializers/category_serializers.py
+++ server/serializers/category_serializers.py
@@ -4,10 +4,17 @@ from server.models import Category
 
 from .utils_serializers import RecursiveSerializer
 
+class FilterParentCategorySerializer(serializers.ListSerializer):
+    def to_representation(self, data):
+        data = data.filter(parent=None)
+        return super().to_representation(data)
+
+
 class CategorySerializer(serializers.ModelSerializer):
     children = RecursiveSerializer(many=True)
 
     class Meta:
+        list_serializer_class = FilterParentCategorySerializer
         model = Category
         fields = ("id", "title", "metaTitle",
                   "slug", "children")
