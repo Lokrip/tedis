59d1184de9c96f9795189197b570ddc52cdad3e4
Lokrip <165576212+Lokrip@users.noreply.github.com>
2025-02-17 20:19:44 +0600

    refactor|feat: improved record creation, added record update, deleted record and changed serilizers

--- server/views/product_view.py
+++ server/views/product_view.py
@@ -2,9 +2,12 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import status
 
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
 from server.serializers.product_serializers import (
     ProductDetailSerializer,
-    ProductCreateSerializer
+    ProductCreateSerializer,
+    ProductUpdateSerializer
 )
 from server.permissions.product_permissions import IsSubscriberOrOwnerEditOrReadOnly
 from server.service.product_service import get_product_list
@@ -20,7 +23,8 @@ from server.mixins import ProductMixin
 class ProductViewSet(ViewSet):
     lookup_field = "slug"
     mixin = ProductMixin(Product)
-    # permission_classes = [IsSubscriberOrOwnerEditOrReadOnly]
+    permission_classes = [IsSubscriberOrOwnerEditOrReadOnly]
+    authentication_classes = [JWTAuthentication]
 
     def list(self, request):
         (serializer, paginator) = self.mixin.findProductAll(
@@ -29,17 +33,15 @@ class ProductViewSet(ViewSet):
         )
         return paginator.get_paginated_response(serializer.data);
 
-    def retrieve(self, request, *args, **kwargs):
-        slug = kwargs.get("slug", None)
-
-        if (slug is None):
+    def retrieve(self, request, slug=None):
+        if slug is None:
             return Response(
                 {"message": DATA_NOT_FOUND},
                 status=status.HTTP_404_NOT_FOUND
             )
 
         try:
-            product = self.mixin.findBySlug(slug=slug, isSlugify=True)
+            product = self.mixin.findBySlug(slug=slug, isSlugify=False)
         except Product.DoesNotExist:
             return Response(
                 {"message": RESOURCE_NOT_FOUND},
@@ -57,8 +59,10 @@ class ProductViewSet(ViewSet):
                 status=status.HTTP_404_NOT_FOUND
             )
         serializer = ProductCreateSerializer(data=data, context={"request": request})
+
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer=serializer)
+
         headers = self.get_success_headers(
             f'/api/v1/products/{serializer.data.get("id")
                 if (serializer.data.get("id", None) is not None)
@@ -73,8 +77,79 @@ class ProductViewSet(ViewSet):
                 "Content-type": "application/json"
             }
         )
-    
+
+    def update(self, request, slug=None):
+        data = request.data or None
+
+        if slug is None or data is None:
+            message = (
+                DATA_NOT_FOUND
+                if data is None
+                else CREATION_FAILED)
+            return Response(
+                {"message": message},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        try:
+            product = self.mixin.findBySlug(slug=slug, isSlugify=False)
+        except (Product.DoesNotExist):
+            return Response(
+                {"message": RESOURCE_NOT_FOUND},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        if data.get("user_id", None) is None or not data.get("user_id", None):
+            data['user_id'] = product.user.pk
+        if data.get("slug", None) is None or not data.get("slug", None):
+            data['slug'] = product.slug
+
+        serializer = ProductUpdateSerializer(instance=product, data=data)
+
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer=serializer)
+
+        headers = self.get_success_headers(
+            f'/api/v1/products/{serializer.data.get("id")
+                if (serializer.data.get("id", None) is not None)
+                else ""
+            }'
+        )
+
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED,
+            headers={
+                **headers,
+                "Content-type": "application/json"
+            }
+        )
+
+    def delete(self, request, slug=None):
+        if slug is None:
+            return Response(
+                {"message": DATA_NOT_FOUND},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        try:
+            product = self.mixin.findBySlug(slug=slug, isSlugify=False)
+        except (Product.DoesNotExist):
+            return Response(
+                {"message": RESOURCE_NOT_FOUND},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        product.delete()
+        return Response(
+            {"message": "Product deleted success"},
+            status=status.HTTP_200_OK
+        )
+
     def perform_create(self, serializer):
+        serializer.save()
+
+    def perform_update(self, serializer):
         serializer.save()
 
     def get_success_headers(self, location):
