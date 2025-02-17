edaf91546e1c83a8e60aa97493b3df21b6e14c9f
Lokrip <165576212+Lokrip@users.noreply.github.com>
2025-02-17 20:24:24 +0600

    style: added a comment for middleware

--- server/middleware.py
+++ server/middleware.py
@@ -5,19 +5,19 @@ class FirstMiddleware:
         self._get_response = get_response
 
     def __call__(self, request):
-        print("Start FirstMiddleware")
+        # print("Start FirstMiddleware")
 
         #можно переврать цепочку middleware
         # return HttpResponse("ok")
         response = self._get_response(request)
-        print("End FirstMiddleware")
-        print(response.status_code)
+        # print("End FirstMiddleware")
+        # print(response.status_code)
         return response;
 
     #он вызываеться когда у нашей view возникает любое не отловлинное исключение
     #тоесть если она возникала джанго у всех middleware вызовит process_exeption
     def process_exeption(self, request, exception):
-        print(f"Exception is {exception}")
+        # print(f"Exception is {exception}")
         return None
 
 class SecondMiddleware:
@@ -25,7 +25,7 @@ class SecondMiddleware:
         self._get_response = get_response
 
     def __call__(self, request):
-        print("Start SecondMiddleware")
+        # print("Start SecondMiddleware")
         response = self._get_response(request)
-        print("End SecondMiddleware")
+        # print("End SecondMiddleware")
         return response;
