5f8193922623e78a01d7b85740d91c1f8d121f3c
Lokrip <165576212+Lokrip@users.noreply.github.com>
2025-02-21 19:20:02 +0600

    fix: delete auto create product and category

--- server/migrations/0004_category_product.py
+++ server/migrations/0004_category_product.py
@@ -8,55 +8,6 @@ from django.db import migrations, models
 
 import random
 
-
-def create_test_data(apps, schema_editor):
-    Category = apps.get_model('server', 'Category')
-    Product = apps.get_model('server', 'Product')
-    User = get_user_model()
-
-    # Проверяем, существуют ли уже категории
-    if Category.objects.exists():
-        print("Категории уже существуют. Пропускаем создание.")
-        return
-
-    # Получаем первого пользователя (или создаем, если его нет)
-    user = User.objects.first()
-    if not user:
-        user = User.objects.create(username='testuser', email='test@example.com')
-
-    # Создаем три категории
-    categories = []
-    for i in range(1, 4):
-        category, created = Category.objects.get_or_create(
-            title=f'Категория {i}',
-            metaTitle=f'Категория {i} Meta',
-            slug=f'category-{i}',
-            parent=None
-        )
-        categories.append(category)
-
-    # Проверяем, существуют ли уже продукты
-    if Product.objects.exists():
-        print("Продукты уже существуют. Пропускаем создание.")
-        return
-
-    # Создаем 100 продуктов
-    for i in range(1, 101):
-        Product.objects.create(
-            title=f'Продукт {i}',
-            metaTitle=f'Продукт {i} Meta',
-            slug=f'product-{i}',
-            summary=f'Описание продукта {i}',
-            accessibility=random.choice(['available', 'out_of_stock', 'coming_soon', 'pre_order']),
-            condition=random.choice(['new', 'used', 'refurbished', 'damaged']),
-            warehouse=random.choice(['reserved', 'in_warehouse', 'out_for_delivery']),
-            promotional=random.choice(['on_sale', 'limited_offer', 'flash_sale', 'black_friday']),
-            checks='approved',
-            category=random.choice(categories),
-            user=user
-        )
-
-
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -105,7 +56,5 @@ class Migration(migrations.Migration):
                 'verbose_name': 'Product',
                 'verbose_name_plural': 'Products',
             },
-        ),
-
-        migrations.RunPython(create_test_data)
+        )
     ]
