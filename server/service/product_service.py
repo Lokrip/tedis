9b9392638d1d55e4a281ed79fd51ba3939c17c19
Lokrip <165576212+Lokrip@users.noreply.github.com>
2025-02-22 19:46:11 +0600

    feat: add filters and search

--- server/service/product_service.py
+++ server/service/product_service.py
@@ -2,9 +2,29 @@ from server.models import Product
 from server.pagination import ProductResultsSetPagination
 from server.serializers.product_serializers import ProductListSerializer
 
+
+def product_filters(query_params, queryset):
+    if not query_params and not isinstance(query_params, dict):
+        raise ValueError("query params must")
+
+    filters = {
+        key: value
+        for key, value in query_params.items()
+        if value is not None
+    }
+
+    query = query_params.get("q")
+
+    if query is not None:
+        queryset = queryset.filter(title__icontains=query)
+        filters.pop("q", None)
+
+    return queryset.filter(**filters)
+
 def get_product_list(**kwargs):
     request = kwargs.get("request", None)
     view = kwargs.get("view")
+    query_params = request.query_params
 
     if request is None:
         raise ValueError("request not found!")
@@ -22,6 +42,8 @@ def get_product_list(**kwargs):
 
     if not queryset.exists():
         return ProductListSerializer([], many=True), ProductResultsSetPagination()
+
+    queryset = product_filters(query_params=query_params, queryset=queryset)
 
     paginator = ProductResultsSetPagination()
     paginated_product = paginator.paginate_queryset(
