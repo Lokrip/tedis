7f09d5d2b9b08bcf9ce83575aa1028fcc7532218
Lokrip <165576212+Lokrip@users.noreply.github.com>
2025-03-05 18:33:27 +0600

    feat: add login modal and change default modal

--- src/components/layouts/Modal/login/LoginModal.tsx
+++ src/components/layouts/Modal/login/LoginModal.tsx
@@ -1,26 +1,37 @@
 "use client"
 
-import {FC} from 'react';
+import {FC, useEffect} from 'react';
 
 import styles from './loginModal.module.scss';
 import Modal from '../Modal';
 import LoginForm from '@/components/ui/form/LoginForm';
-import { useAppSelector } from '@/hooks';
+import { useActions, useAppSelector } from '@/hooks';
+import { useRouter, useSearchParams } from 'next/navigation';
+import pages from '@/service/route';
 
 interface LoginModalProps {}
 
 const LoginModal: FC<LoginModalProps> = () => {
-    const {isModalClose} = useAppSelector((state) => state.utilsReducer)
+    const {modalClose} = useActions()
+    const {isModalClose, isModalOpen} = useAppSelector((state) => state.utilsReducer)
+    const searchParam = useSearchParams()
+    const callbackRoute = searchParam.get("callback") || pages.home
+    const router = useRouter()
+
+    const onClick = () => {
+        if (callbackRoute) {
+            router.push(callbackRoute)
+        } else {
+            router.push(pages.home)
+        }
+        modalClose()
+    }
 
     return (
         <>
         {!isModalClose && (
-            <Modal>
-                <div className={styles.loginModalContainer}>
-                    <div className={styles.loginModal}>
-                        <LoginForm />
-                    </div>
-                </div>
+            <Modal onClick={onClick}>
+                <LoginForm />
             </Modal>
         )}
         </>
