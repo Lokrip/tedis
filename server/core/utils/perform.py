131cd9b2e4cce2cade4cc73fb05e3cd42c019035
Lokrip <165576212+Lokrip@users.noreply.github.com>
2025-03-30 19:57:44 +0600

    fix: rename deleted perform

--- server/core/utils/perform.py
+++ server/core/utils/perform.py
@@ -6,11 +6,11 @@ class PerformUpdated:
     def perform_updated(self, instance):
         instance.save()
 
-class PerformDeleted:
-    def perform_deleted(self, instance):
+class PerformDestroy:
+    def perform_destroy(self, instance):
         instance.delete()
 
 class PerformBase(PerformCreated,
                   PerformUpdated,
-                  PerformDeleted):
+                  PerformDestroy):
     pass
