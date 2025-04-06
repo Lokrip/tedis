14d2e86981464aed900ea9b9c870c2537398592a
Lokrip <165576212+Lokrip@users.noreply.github.com>
2025-04-06 20:54:45 +0600

    lint: improved product service

--- server/views/product_view.py
+++ server/views/product_view.py
@@ -21,7 +21,7 @@ class ProductViewSet(ViewSet):
             request=request
         )
         if paginator:
-            return paginator.get_paginated_response(serializer.data);
+            return paginator.get_paginated_response(serializer.data)
         else:
             return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -44,10 +44,7 @@ class ProductViewSet(ViewSet):
                 status=status.HTTP_404_NOT_FOUND
             )
         headers = self.get_success_headers(
-            f'/api/v1/products/{serializer.data.get("id")
-                if (serializer.data.get("id", None) is not None)
-                else ""
-            }'
+            f'/api/v1/products/{serializer.data.get("id") if (serializer.data.get("id", None) is not None) else ""}'
         )
         return Response(
             serializer.data,
