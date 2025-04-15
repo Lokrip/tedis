37b980c3c040e860fb64e187b172e5bd6d607cd0
Lokrip <slava.dorokhov.08@mail.ru>
2025-04-15 18:27:24 +0600

    refactor: improved run_server

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 set -e
 
+DB_HOST=${DB_HOST:-db-54321}
+DB_PORT=${DB_PORT:-5432}
+
 log() {
     echo "[INFO] $(date +'%Y-%m-%d %H:%M:%S') - $1"
 }
 
-log "Запуск сервера..."
+log "Ожидание доступности базы данных на ${DB_HOST}:${DB_PORT}..."
 
-sleep 5
-echo "PostgreSQL доступен! Запускаем приложение..."
+while ! nc -z $DB_HOST $DB_PORT; do
+  echo "Ожидание PostgreSQL на ${DB_HOST}:${DB_PORT}..."
+  sleep 1
+done
+
+log "PostgreSQL доступен! Запускаем приложение..."
 
 make makemigrations
 make migrate
